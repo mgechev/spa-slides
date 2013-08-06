b493a9824d1fabc66576f24a9cd579c1a8e95f86
mgechev <mgechev@gmail.com>
2013-08-06 17:20:48 +0300

    Improvements

--- demo/complete/js/app/app.js
+++ demo/complete/js/app/app.js
@@ -5,10 +5,12 @@
         .when('/', {
             templateUrl: './partials/laptops-list.html',
             controller: 'LaptopsListCtrl'
+            /*  Resolve should be included: http://docs.angularjs.org/api/ng.$routeProvider  */
         })
         .when('/laptop/:id', {
             templateUrl: './partials/laptop-details.html',
             controller: 'LaptopDetailsCtrl'
+            /*  Resolve should be included: http://docs.angularjs.org/api/ng.$routeProvider  */
         })
         .otherwise({
             templateUrl: './partials/404.html'
