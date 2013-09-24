c2aef33f06f2bf86bd07207a052c399225d85d36
mgechev <mgechev@gmail.com>
2013-09-24 12:30:18 +0300

    Gruntfile and package.json

---
+++ Gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function (grunt) {
+  grunt.loadNpmTasks('grunt-contrib-connect');
+
+  grunt.initConfig({
+    connect: {
+      server: {
+        options: {
+          port: 8080,
+          base: '.'
+        }
+      }
+    }
+  });
+};
+
