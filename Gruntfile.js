5df47927d979c6677c5b27c70d88b65669c16f52
mgechev <mgechev@gmail.com>
2013-09-24 12:46:47 +0300

    Few dependencies added

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,7 @@
 module.exports = function (grunt) {
+
   grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-bower-task');
 
   grunt.initConfig({
     connect: {
@@ -7,6 +9,17 @@ module.exports = function (grunt) {
         options: {
           port: 8080,
           base: '.'
+        }
+      }
+    },
+    bower: {
+      install: {
+        options: {
+          targetDir: './lib',
+          layout: 'byType',
+          install: true,
+          verbose: true,
+          cleanBowerDir: true
         }
       }
     }
